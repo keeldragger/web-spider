78bfb7b9d6a77e73d0715c2fa232f686ae5e051b
jdereg <jdereg@gmail.com>
2015-03-08 11:16:16 -0400

    - version 0.02, breaking out main routines

--- src/main/groovy/com/cedarsoftware/webspider/WebSpider.groovy
+++ src/main/groovy/com/cedarsoftware/webspider/WebSpider.groovy
@@ -1,20 +1,21 @@
 package com.cedarsoftware.webspider
 
+import com.cedarsoftware.util.StringUtilities
 import com.cedarsoftware.util.UrlUtilities
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 /**
  * Created by jderegnaucourt on 2015/03/08.
  */
 class WebSpider
 {
+    static final String USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    private static final Logger LOG = LogManager.getLogger(WebSpider.class);
+
     WebSpider()
     {
-    }
-
-    static void main(String[] args)
-    {
-        WebSpider spider = new WebSpider()
-        spider.crawl()
+        UrlUtilities.setUserAgent(USER_AGENT);
     }
 
     void crawl()
@@ -34,13 +35,33 @@ class WebSpider
 
             visited.add(url)
 
-            byte[] content = UrlUtilities.getContentFromUrl(url, null, null, true)
-
-            String s = new String(content)
-            println url
-            println '--------------------------------------------------------------------------------------------------'
-            println s
-            println '=================================================================================================='
+            String content = fetch(url)
+            List<String> urls = extractUrls(content)
+            stack.addAll(urls)
         }
+    }
+
+    String fetch(String url)
+    {
+        byte[] content = UrlUtilities.getContentFromUrl(url, null, null, true)
+        if (content == null)
+        {
+            return ''
+        }
+
+        try
+        {
+            return StringUtilities.createString(content, 'UTF-8')
+        }
+        catch (Exception e)
+        {
+            LOG.warn('Error creating string from content, url: ' + url, e)
+            return ''
+        }
+    }
+
+    List<String> extractUrls(String content)
+    {
+
     }
 }
